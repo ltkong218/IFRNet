1b1cb0d64122554aff8a17aff7db0b06d1dcf10d
Tone <34116113+ltkong218@users.noreply.github.com>
2022-07-09 17:26:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ import re
 import numpy as np
 from imageio import imread, imwrite
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
