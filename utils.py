8fdd6de517677ba0762e5914ddb2408b245faa1b
Jared Chan <jaredchan42@gmail.com>
2024-01-08 01:17:23 +0000

    Make ifrnet compatible with the converter

--- utils.py
+++ utils.py
@@ -11,12 +11,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def warp(img, flow):
-    B, _, H, W = flow.shape
-    xx = torch.linspace(-1.0, 1.0, W).view(1, 1, 1, W).expand(B, -1, H, -1)
-    yy = torch.linspace(-1.0, 1.0, H).view(1, 1, H, 1).expand(B, -1, -1, W)
+def warp(img, flow, flow_shape):
+    # B, _, H, W = flow_shape
+    xx = torch.linspace(-1.0, 1.0, flow_shape[3]).view(1, 1, 1, flow_shape[3]).expand(flow_shape[0], -1, flow_shape[2], -1)
+    yy = torch.linspace(-1.0, 1.0, flow_shape[2]).view(1, 1, flow_shape[2], 1).expand(flow_shape[0], -1, -1, flow_shape[3])
     grid = torch.cat([xx, yy], 1).to(img)
-    flow_ = torch.cat([flow[:, 0:1, :, :] / ((W - 1.0) / 2.0), flow[:, 1:2, :, :] / ((H - 1.0) / 2.0)], 1)
+    flow_ = torch.cat([flow[:, 0:1, :, :] / ((flow_shape[3] - 1.0) / 2.0), flow[:, 1:2, :, :] / ((flow_shape[2] - 1.0) / 2.0)], 1)
     grid_ = (grid + flow_).permute(0, 2, 3, 1)
     output = F.grid_sample(input=img, grid=grid_, mode='bilinear', padding_mode='border', align_corners=True)
     return output
